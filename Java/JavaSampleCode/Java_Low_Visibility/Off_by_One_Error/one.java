8c22a5035948145c2ccd0a800360914fbf53d172
Happy Yang <happy.yang@checkmarx.com>
2021-07-07 13:32:20 +0800

    Java_Low_Visibility/Off_by_One_Error

--- Java/JavaSampleCode/Java_Low_Visibility/Off_by_One_Error/one.java
+++ Java/JavaSampleCode/Java_Low_Visibility/Off_by_One_Error/one.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class one {
+    public static final int s = 0;
 
     public void main() {
         List<String> eventTypeList = new ArrayList<String>(Arrays.asList("some one", "second one"));
@@ -12,6 +13,10 @@ public class one {
         while (i <= eventTypeList.size()) {
 
             expectedEvent = eventTypeList.get(i - 1);
+        }
+
+        while (i < accounts_reserves.size()) {
+            String something = accounts_reserves.get(i - 1).getSacDisplayTitle();
         }
 
     }
